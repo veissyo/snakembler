db112eb8b0aad187d99e24770d05b45911204a13
veissyo <martyna.danilczykk6@gmail.com>
2026-07-30 19:11:37 +0200

    Add wall collisions and more snake segments.

--- snake.s
+++ snake.s
@@ -13,6 +13,7 @@
 .equ STDOUT, 1
 
 .equ ROW_LEN, 21
+.equ MAX_SNAKE, 100
 
 .text
 _start:
@@ -47,6 +48,20 @@ copy_loop:
 	mov x8, #SYS_ioctl
 	svc #0
 
+	ldr x0, =snake_body_x
+	mov w1, #12
+	strb w1, [x0]
+	mov w1, #11
+	strb w1, [x0, #1]
+	mov w1, #10
+	strb w1, [x0, #2]
+
+	ldr x0, =snake_body_y
+	mov w1, #5
+	strb w1, [x0]
+	strb w1, [x0, #1]
+	strb w1, [x0, #2]
+
 read_loop:
 	mov x0, #STDOUT
 	ldr x1, =clear_screen
@@ -63,12 +78,35 @@ copy_board_loop:
 	subs x2, x2, #1
 	bne copy_board_loop
 
+	ldr x0, =snake_length
+	ldrb w1, [x0]
+	sub x1, x1, #1
+	cmp x1, #0
+	ble shift_done
+
+	ldr x2, =snake_body_x
+	ldr x3, =snake_body_y
+	mov x4, x1
+
+shift_loop:
+	sub x5, x4, #1
+	ldrb w6, [x2, x5]
+	strb w6, [x2, x4]
+	ldrb w6, [x3, x5]
+	strb w6, [x3, x4]
+
+	subs x4, x4, #1
+	cmp x4, #0
+	bgt shift_loop
+
+shift_done:
+
 	ldr x0, =direction
 	ldrb w1, [x0]
 
 	cmp w1, #0
 	bne move_check_down
-	ldr x0, =snake_y
+	ldr x0, =snake_body_y
 	ldrb w2, [x0]
 	sub w2, w2, #1
 	strb w2, [x0]
@@ -76,7 +114,7 @@ copy_board_loop:
 move_check_down:
 	cmp w1, #1
 	bne move_check_left
-	ldr x0, =snake_y
+	ldr x0, =snake_body_y
 	ldrb w2, [x0]
 	add w2, w2, #1
 	strb w2, [x0]
@@ -84,31 +122,58 @@ move_check_down:
 move_check_left:
 	cmp w1, #2
 	bne move_check_right
-	ldr x0, =snake_x
+	ldr x0, =snake_body_x
 	ldrb w2, [x0]
 	sub w2, w2, #1
 	strb w2, [x0]
 	b move_done
 move_check_right:
-	ldr x0, =snake_x
+	ldr x0, =snake_body_x
 	ldrb w2, [x0]
 	add w2, w2, #1
 	strb w2, [x0]
 move_done:
 
-	ldr x0, =snake_y
-	ldrb w4, [x0]
+	ldr x0, =snake_body_x
+	ldrb w1, [x0]
+	cmp w1, #1
+	blt game_over
+	cmp w1, #18
+	bgt game_over
+
+	ldr x0, =snake_body_y
+	ldrb w1, [x0]
+	cmp w1, #1
+	blt game_over
+	cmp w1, #8
+	bgt game_over
+
+	mov x9, #0
+	ldr x10, =snake_length
+	ldrb w10, [x10]
+
+draw_loop:
+	cmp x9, x10
+	bge draw_done
+
+	ldr x0, =snake_body_y
+	ldrb w4, [x0, x9]
 	mov x5, #ROW_LEN
 	mul x4, x4, x5
 
-	ldr x0, =snake_x
-	ldrb w5, [x0]
+	ldr x0, =snake_body_x
+	ldrb w5, [x0, x9]
 	add x4, x4, x5
 
 	ldr x6, =work_board
 	add x6, x6, x4
-	mov w7, #'0'
+	mov w7, #'o'
 	strb w7, [x6]
+
+	add x9, x9, #1
+	b draw_loop
+
+draw_done:
 
 	mov x0, #STDOUT
 	ldr x1, =work_board
@@ -165,6 +230,13 @@ no_key:
 
 	b read_loop
 
+game_over:
+	mov x0, #STDOUT
+	ldr x1, =game_over_msg
+	mov x2, #game_over_len
+	mov x8, #SYS_write
+	svc #0
+
 end_loop:
 
 	mov x0, #STDIN
@@ -208,9 +280,16 @@ board_len = . - board
 work_board:
 	.skip board_len
 
-snake_x:
-	.byte 10
-snake_y:
-	.byte 5
+snake_body_x:
+	.skip MAX_SNAKE
+snake_body_y:
+	.skip MAX_SNAKE
+snake_length:
+	.byte 3
 direction:
 	.byte 3
+game_over_msg:
+	.ascii "\nGAME OVER\n"
+game_over_len = . - game_over_msg
+
+
